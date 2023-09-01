f6994d1740cb2b232104f7478604a77c1f16090a
Elena12885 <elena.emshanova.12885@gmail.com>
2023-09-01 10:33:16 +0300

    added name request in src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from "readline-sync";
+
+const nameUser = () => { 
+    const name = readlineSync.question("May I have your name? ");
+    return name;
+};
+
+export default nameUser;
