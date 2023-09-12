cbe54a8eb72cc065978124ea57445b30f9d2d7e0
Elena12885 <elena.emshanova.12885@gmail.com>
2023-09-12 19:16:39 +0300

    added game brain-even

---
+++ src/even.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const determiningTheParity = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+const userResponse = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const random = getRandomInt(100);
+    const determining = determiningTheParity(random);
+    console.log(`Question: ${random}`);
+    const response = readlineSync.question('Your answer: ');
+    if (response !== determining) {
+      console.log(`${response} is wrong answer ;(. Correct answer was ${determining}.`);
+      return false;
+    }
+    console.log('Correct!');
+  }
+  return true;
+};
+
+const brainEven = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  if (userResponse() !== true) {
+    return (`Let's try again, ${name}!`);
+  }
+  return (`Congratulations, ${name}!`);
+};
+
+export default brainEven;
