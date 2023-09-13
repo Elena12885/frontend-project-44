b3f8538b441c0bcf4f30ec87c788a939de413462
Elena12885 <elena.emshanova.12885@gmail.com>
2023-09-13 21:07:01 +0300

    fixed files even.js and brain-even.js

--- src/even.js
+++ src/even.js
@@ -25,6 +25,7 @@ const userResponse = () => {
 };
 
 const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
